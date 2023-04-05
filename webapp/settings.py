ff497d5aea91117479fb81176ac0033f13554404
elsunais6167 <yelsunais2014@gmail.com>
2023-04-05 10:55:16 +0100

    database

--- webapp/settings.py
+++ webapp/settings.py
@@ -131,8 +131,10 @@ MEDIA_ROOT = '/media/'
 LOGIN_URL = '/login/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static_files')
 ]
 
 
